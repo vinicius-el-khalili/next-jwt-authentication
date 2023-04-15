7ebf89ded98d1535d27b8130cf3d191a56a9a98d
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2023-04-14 22:24:08 -0300

    initiating fake server for testing

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -3,9 +3,14 @@ import { createContext } from "react";
 export const AuthContext = createContext({})
 
 export function AuthProvider({children}) {
+    const isAuthenticated = false
+    async function signIn(){
+        
+    }
+
     return (
         <AuthContext.Provider value={{
-            
+            isAuthenticated
         }}>
 
             {children}
